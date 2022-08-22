d4a8bac3a801b4bbad2a5f4182561387a6a111a7
Chris Athanas <realityexpander@gmail.com>
2022-08-21 20:49:35 -0500

    update to exoplayer 2.18.1

--- app/src/main/java/com/realityexpander/basicexoplayer2/MainActivity.kt
+++ app/src/main/java/com/realityexpander/basicexoplayer2/MainActivity.kt
@@ -8,23 +8,29 @@ import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.source.hls.DefaultHlsDataSourceFactory
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     var simpleExoPlayer: SimpleExoPlayer? = null
+    var exoPlayer: ExoPlayer? = null
     var playerView: PlayerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initPlayer()
+//        initPlayer() // for exoplayer 2.13.3
+        initPlayer2() // for exoplayer 2.18.1
     }
 
+    // For 'com.google.android.exoplayer:exoplayer:2.13.3'
     fun initPlayer() {
         simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
         playerView = findViewById(R.id.playerView)
@@ -38,8 +44,39 @@ class MainActivity : AppCompatActivity() {
         val dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, "BasicExoPlayer"))
         val audioSource = ProgressiveMediaSource.Factory(dataSourceFactory)
             .createMediaSource(MediaItem.fromUri(Uri.parse("https://files.freemusicarchive.org//storage-freemusicarchive-org//tracks//Pwgnnzp2ZsICaklopTbKD24keSTqsptGRvZSmY2J.mp3")))
-        simpleExoPlayer?.setMediaSource(audioSource)
-        simpleExoPlayer?.prepare()
-        simpleExoPlayer?.playWhenReady = true
+        simpleExoPlayer?.apply {
+            setMediaSource(audioSource)
+            prepare()
+            playWhenReady = true
+        }
+    }
+
+    // for 'com.google.android.exoplayer:exoplayer:2.18.1'
+    fun initPlayer2() {
+        playerView = findViewById(R.id.playerView)
+
+        exoPlayer = ExoPlayer.Builder(this).build()
+        exoPlayer?.playWhenReady = true
+        playerView?.player = exoPlayer
+
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem =
+            MediaItem.fromUri("https://files.freemusicarchive.org//storage-freemusicarchive-org//tracks//Pwgnnzp2ZsICaklopTbKD24keSTqsptGRvZSmY2J.mp3")
+//            MediaItem.fromUri("https://www.djing.com/tv/28676-04.m3u8")
+
+        // for HLS (m3u8)
+        val mediaSource =
+            HlsMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+
+        // for Progressive (mp3)
+        val mediaSource2 = ProgressiveMediaSource.Factory(defaultHttpDataSourceFactory)
+            .createMediaSource(mediaItem)
+
+        exoPlayer?.apply {
+            setMediaSource(mediaSource2)
+            seekTo(0)
+            playWhenReady = playWhenReady
+            prepare()
+        }
     }
 }
